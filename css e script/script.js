b6820adb853b16461c9fb671d1c461afb74645c4
kassioba <kassioluna@gmail.com>
2023-01-20 12:32:49 -0300

    recebimento de mensagens implementado

--- css e script/script.js
+++ css e script/script.js
@@ -16,10 +16,10 @@ function entrarSala() {
   requisicaoNome.catch(falhou);
 }
 
-function sucesso(dados) {
-  chat.innerHTML += `<div class="mensagem-status"><span class="hora">(09:22:28)</span>  <span class="nome">${nome[0].name}</span>  entra na sala...</div>`;
-
+function sucesso() {
   setInterval(afirmarPresenca, 5000);
+  buscarMensagem();
+  setInterval(buscarMensagem, 3000);
 }
 
 function falhou(erro) {
@@ -44,4 +44,51 @@ function nomeIndisponivel() {
 
 function afirmarPresenca() {
   axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nome[0]);
+}
+
+function buscarMensagem() {
+  const promessa = axios.get(
+    "https://mock-api.driven.com.br/api/v6/uol/messages"
+  );
+
+  promessa.then(sucessoBM);
+  promessa.catch(falhouBM);
+}
+
+let mensagemModeloStatus = undefined;
+let mensagemModeloMessage = undefined;
+let mensagemModeloPrivMessage = undefined;
+
+function sucessoBM(mensagem) {
+  console.log(mensagem.data);
+
+  chat.innerHTML = "";
+  for (let i = 0; i < mensagem.data.length; i++) {
+    mensagemModeloStatus = `
+    <div class="mensagem-status">
+    <span class="hora">(${mensagem.data[i].time})</span>  <span class="nome">${mensagem.data[i].from}</span>  ${mensagem.data[i].text}
+    </div>`;
+
+    mensagemModeloMessage = `
+    <div class="mensagem-normal">
+    <span class="hora">(${mensagem.data[i].time})</span> <span class="nome">${mensagem.data[i].from}</span> para <span class="nome">${mensagem.data[i].to}</span>:  ${mensagem.data[i].text}
+    </div>`;
+
+    mensagemModeloPrivMessage = `
+    <div class="mensagem-reservada">
+    <span class="hora">(${mensagem.data[i].time})</span>  <span class="nome">${mensagem.data[i].from}</span> reservadamente para <span class="nome">${mensagem.data[i].to}</span>:  ${mensagem.data[i].text}
+    </div>`;
+
+    if (mensagem.data[i].type === "status") {
+      chat.innerHTML += mensagemModeloStatus;
+    } else if (mensagem.data[i].type === "message") {
+      chat.innerHTML += mensagemModeloMessage;
+    } else if (mensagem.data[i].type === "private_message") {
+      chat.innerHTML += mensagemModeloPrivMessage;
+    }
+  }
+}
+console.log(mensagemModeloStatus);
+function falhouBM(a) {
+  console.log("buscar mensagens falhou - " + a.data);
 }
